22ce926cb6fa013a23d6605f32622252b5041b08
vicsaa01 <victorsaakes@gmail.com>
2024-11-18 13:31:52 +0100

    #002 - getting started

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { TechComponent } from './tech/tech.component'
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent },
+    {path: 'tech-stack', component: TechComponent }
+];
