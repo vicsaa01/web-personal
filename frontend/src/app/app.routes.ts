502b880d7ef89d4afd1c7f15d76dc74db35f68ae
vicsaa01 <victorsaakes@gmail.com>
2024-11-25 14:21:00 +0100

    #005 - nav, footer

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
-import { TechComponent } from './tech/tech.component'
+import { ProjectsPage } from './projects-page/projects-page.component'
+import { CVPage } from './cv-page/cv-page.component'
+import { ContactPage } from './contact-page/contact-page.component'
 
 export const routes: Routes = [
     {path: '', component: HomeComponent },
-    {path: 'tech-stack', component: TechComponent }
+    {path: 'projects', component: ProjectsPage },
+    {path: 'cv', component: CVPage },
+    {path: 'contact', component: ContactPage }
 ];
--- frontend/src/app/contact-page/contact-page.component.spec.ts
+++ frontend/src/app/contact-page/contact-page.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TechComponent } from './tech.component';
+import { ContactPageComponent } from './contact-page.component';
 
-describe('TechComponent', () => {
-  let component: TechComponent;
-  let fixture: ComponentFixture<TechComponent>;
+describe('ContactPageComponent', () => {
+  let component: ContactPageComponent;
+  let fixture: ComponentFixture<ContactPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TechComponent]
+      imports: [ContactPageComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TechComponent);
+    fixture = TestBed.createComponent(ContactPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
